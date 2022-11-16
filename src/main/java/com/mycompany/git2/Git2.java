b4196f952eda984abeb7eef2a4f169ee0b2e688e
elkik <elkik@LAPTOP-FDAOLACM>
2022-11-16 15:11:59 -0600

    Jueves 16/11/2022

--- src/main/java/com/mycompany/git2/Git2.java
+++ src/main/java/com/mycompany/git2/Git2.java
@@ -12,6 +12,6 @@ package com.mycompany.git2;
 public class Git2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hoy es jueves 16/11/2022");
     }
 }
